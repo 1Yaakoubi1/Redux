42eef426110c9f0df58e318d15c2e03b4164b92a
unknown <saber.yacoubi@gmail.com>
2022-01-24 15:27:53 +0100

    first commit

---
+++ src/component/Task.js
@@ -0,0 +1,20 @@
+import { Button } from "react-bootstrap";
+import { useDispatch } from 'react-redux'
+import { deleteTask, toggleDone } from "../Redux/actions/todoActions.js";
+
+import EditTask from "./EditTask";
+
+function Task({ el }) {
+    const dispatch = useDispatch()
+    return (
+        <div style={{ display: 'flex' }}>
+            <p style={{ color: el.done ? "green" : null }} > {el.task} </p>
+            <Button variant="outline-success" onClick={() => dispatch(toggleDone(el.id))} >  Done</Button>{' '}
+            <Button variant="outline-danger" onClick={() => dispatch(deleteTask(el.id))} > Delete</Button>{' '}
+            <EditTask el={el} />
+        </div>
+
+    )
+}
+
+export default Task
